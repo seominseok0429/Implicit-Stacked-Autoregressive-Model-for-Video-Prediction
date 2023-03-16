c91b1b4d56a6994f84f3f06389a517c980194c4c
seominseok0429 <king_v_@naver.com>
2023-03-16 05:44:14 +0000

    IAM4VP

---
+++ model.py
@@ -0,0 +1,154 @@
+import torch
+import math
+import random
+from torch import nn
+from modules import ConvSC, ConvNeXt_block, Learnable_Filter, Attention, ConvNeXt_bottle
+
+class SinusoidalPosEmb(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, x):
+        device = x.device
+        half_dim = self.dim // 2
+        emb = math.log(10000) / (half_dim - 1)
+        emb = torch.exp(torch.arange(half_dim, device=device) * -emb)
+        emb = x[:, None] * emb[None, :]
+        emb = torch.cat((emb.sin(), emb.cos()), dim=-1)
+        return emb
+
+class Time_MLP(nn.Module):
+    def __init__(self, dim):
+        super(Time_MLP, self).__init__()
+        self.sinusoidaposemb = SinusoidalPosEmb(dim)
+        self.linear1 = nn.Linear(dim, dim*4)
+        self.gelu = nn.GELU()
+        self.linear2 = nn.Linear(dim*4, dim)
+
+    def forward(self, x):
+        x = self.sinusoidaposemb(x)
+        x = self.linear1(x)
+        x = self.gelu(x)
+        x = self.linear2(x)
+        return x
+
+def stride_generator(N, reverse=False):
+    strides = [1, 2]*10
+    if reverse: return list(reversed(strides[:N]))
+    else: return strides[:N]
+
+class Encoder(nn.Module):
+    def __init__(self,C_in, C_hid, N_S):
+        super(Encoder,self).__init__()
+        strides = stride_generator(N_S)
+        self.enc = nn.Sequential(
+            ConvSC(C_in, C_hid, stride=strides[0]),
+            *[ConvSC(C_hid, C_hid, stride=s) for s in strides[1:]]
+        )
+    
+    def forward(self,x):# B*4, 3, 128, 128
+        enc1 = self.enc[0](x)
+        latent = enc1
+        for i in range(1,len(self.enc)):
+            latent = self.enc[i](latent)
+        return latent,enc1
+
+
+class Decoder(nn.Module):
+    def __init__(self,C_hid, C_out, N_S):
+        super(Decoder,self).__init__()
+        strides = stride_generator(N_S, reverse=True)
+        self.dec = nn.Sequential(
+            *[ConvSC(C_hid, C_hid, stride=s, transpose=True) for s in strides[:-1]],
+            ConvSC(2*C_hid, C_hid, stride=strides[-1], transpose=True)
+        )
+        self.readout = nn.Conv2d(640, 64, 1)
+    
+    def forward(self, hid, enc1=None):
+        for i in range(0,len(self.dec)-1):
+            hid = self.dec[i](hid)
+        Y = self.dec[-1](torch.cat([hid, enc1], dim=1))
+        ys =Y.shape
+        Y = Y.reshape(int(ys[0]/10), int(ys[1]*10), 64, 64)
+        Y = self.readout(Y)
+        return Y
+
+class Predictor(nn.Module):
+    def __init__(self, channel_in, channel_hid, N_T):
+        super(Predictor, self).__init__()
+
+        self.N_T = N_T
+        st_block = [ConvNeXt_bottle(dim=channel_in)]
+        for i in range(0, N_T):
+            st_block.append(ConvNeXt_block(dim=channel_in))
+
+        self.st_block = nn.Sequential(*st_block)
+
+    def forward(self, x, time_emb):
+        B, T, C, H, W = x.shape
+        x = x.reshape(B, T*C, H, W)
+        z = self.st_block[0](x, time_emb)
+        for i in range(1, self.N_T):
+            z = self.st_block[i](z, time_emb)
+
+        y = z.reshape(B, int(T/2), C, H, W)
+        return y
+
+class IAM4VP(nn.Module):
+    def __init__(self, shape_in, hid_S=64, hid_T=512, N_S=4, N_T=6):
+        super(IAM4VP, self).__init__()
+        T, C, H, W = shape_in
+        self.time_mlp = Time_MLP(dim=64)
+        self.enc = Encoder(C, hid_S, N_S)
+        self.hid = Predictor(T*hid_S, hid_T, N_T)
+        self.dec = Decoder(hid_S, C, N_S)
+        self.attn = Attention(64)
+        self.readout = nn.Conv2d(64, 1, 1)
+        self.mask_token = nn.Parameter(torch.zeros(10, hid_S, 16, 16))
+
+    def random_masking(self, z, t):
+        B, T, C , H, W = z.shape
+        for i in range(B):
+            rand_index = random.choices([0,1], weights=[0.5, 0.5], k=10)
+            for index, j in enumerate(rand_index):
+                if j==0:
+                    z[i,index,:,:,:] = self.mask_token[index,:,:,:]
+                if index >= int(t[i]/100):
+                    z[i,index,:,:,:] = self.mask_token[index,:,:,:]
+        return z
+
+    def forward(self, x_raw, y_raw=None, t=None):
+        B, T, C, H, W = x_raw.shape
+        x = x_raw.view(B*T, C, H, W)
+        y = y_raw.view(B*T, C, H, W)
+
+        time_emb = self.time_mlp(t)
+
+        embed, skip = self.enc(x)
+        embed2, _ = self.enc(x)
+
+        _, C_, H_, W_ = embed.shape
+
+        z = embed.view(B, T, C_, H_, W_)
+        z2 = embed2.view(B, T, C_, H_, W_)
+
+        z2 = self.random_masking(z2, t)
+        z = torch.cat([z, z2], dim=1)
+        hid = self.hid(z, time_emb)
+        hid = hid.reshape(B*T, C_, H_, W_)
+
+        Y = self.dec(hid, skip)
+        Y = self.attn(Y)
+        Y = self.readout(Y)
+        return Y
+
+if __name__ == "__main__":
+    import numpy as np
+    model = IAM4VP([10,1,64,64])
+    inputs = torch.randn(2,10, 1,64,64)
+    inputs2 = torch.randn(2,10, 1,64,64)
+    t = np.random.choice(10, 2)
+    t = torch.tensor(t)*100
+    b = model(x_raw=inputs, y_raw=inputs2, t=t)
+    print(b.shape)
---
+++ modules.py
@@ -0,0 +1,290 @@
+import torch
+import torch.nn.functional as F
+from einops import rearrange
+from torch import nn
+from timm.models.layers import trunc_normal_
+
+class LayerNorm(nn.Module):
+    r""" LayerNorm that supports two data formats: channels_last (default) or channels_first.
+    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with
+    shape (batch_size, height, width, channels) while channels_first corresponds to inputs
+    with shape (batch_size, channels, height, width).
+    """
+    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_last"):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.eps = eps
+        self.data_format = data_format
+        if self.data_format not in ["channels_last", "channels_first"]:
+            raise NotImplementedError
+        self.normalized_shape = (normalized_shape, )
+
+    def forward(self, x):
+        if self.data_format == "channels_last":
+            return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
+        elif self.data_format == "channels_first":
+            u = x.mean(1, keepdim=True)
+            s = (x - u).pow(2).mean(1, keepdim=True)
+            x = (x - u) / torch.sqrt(s + self.eps)
+            x = self.weight[:, None, None] * x + self.bias[:, None, None]
+            return x
+
+class BasicConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, dilation=1, transpose=False, act_norm=False):
+        super(BasicConv2d, self).__init__()
+        self.act_norm = act_norm
+        if transpose is True:
+            self.conv = nn.Sequential(*[
+                nn.Conv2d(in_channels, out_channels*4, kernel_size=kernel_size,
+                          stride=1, padding=padding, dilation=dilation),
+                nn.PixelShuffle(2)
+            ])
+        else:
+            self.conv = nn.Conv2d(
+                in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation)
+        self.norm = LayerNorm(out_channels, eps=1e-6, data_format="channels_first")
+        self.act = nn.SiLU(True)
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, (nn.Conv2d)):
+            trunc_normal_(m.weight, std=.02)
+            nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        y = self.conv(x)
+        if self.act_norm:
+            y = self.act(self.norm(y))
+        return y
+
+
+
+class ConvSC(nn.Module):
+    def __init__(self, C_in, C_out, stride, transpose=False, act_norm=True):
+        super(ConvSC, self).__init__()
+        if stride == 1:
+            transpose = False
+        self.conv = BasicConv2d(C_in, C_out, kernel_size=3, stride=stride,
+                                padding=1, transpose=transpose, act_norm=act_norm)
+
+    def forward(self, x):
+        y = self.conv(x)
+        return y
+
+class ConvNeXt_block(nn.Module):
+    r""" ConvNeXt Block. There are two equivalent implementations:
+    (1) DwConv -> LayerNorm (channels_first) -> 1x1 Conv -> GELU -> 1x1 Conv; all in (N, C, H, W)
+    (2) DwConv -> Permute to (N, H, W, C); LayerNorm (channels_last) -> Linear -> GELU -> Linear; Permute back
+    We use (2) as we find it slightly faster in PyTorch
+    
+    Args:
+        dim (int): Number of input channels.
+        drop_path (float): Stochastic depth rate. Default: 0.0
+        layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
+    """
+    def __init__(self, dim, drop_path=0., layer_scale_init_value=1e-6):
+        super().__init__()
+        self.mlp = nn.Sequential(
+            nn.GELU(),
+            nn.Linear(64, dim)
+        )
+        self.dwconv = nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim) # depthwise conv
+        self.norm = LayerNorm(dim, eps=1e-6)
+        self.pwconv1 = nn.Linear(dim, 4 * dim) # pointwise/1x1 convs, implemented with linear layers
+        self.act = nn.GELU()
+        self.pwconv2 = nn.Linear(4 * dim, dim)
+        self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim)), 
+                                    requires_grad=True) if layer_scale_init_value > 0 else None
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+    def forward(self, x, time_emb=None):
+        input = x
+        time_emb = self.mlp(time_emb)
+        x = self.dwconv(x) +  rearrange(time_emb, 'b c -> b c 1 1')
+        x = x.permute(0, 2, 3, 1) # (N, C, H, W) -> (N, H, W, C)
+        x = self.norm(x)
+        x = self.pwconv1(x)
+        x = self.act(x)
+        x = self.pwconv2(x)
+        if self.gamma is not None:
+            x = self.gamma * x
+        x = x.permute(0, 3, 1, 2) # (N, H, W, C) -> (N, C, H, W)
+
+        x = input + self.drop_path(x)
+        return x
+
+class ConvNeXt_bottle(nn.Module):
+    r""" ConvNeXt Block. There are two equivalent implementations:
+    (1) DwConv -> LayerNorm (channels_first) -> 1x1 Conv -> GELU -> 1x1 Conv; all in (N, C, H, W)
+    (2) DwConv -> Permute to (N, H, W, C); LayerNorm (channels_last) -> Linear -> GELU -> Linear; Permute back
+    We use (2) as we find it slightly faster in PyTorch
+
+    Args:
+        dim (int): Number of input channels.
+        drop_path (float): Stochastic depth rate. Default: 0.0
+        layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
+    """
+    def __init__(self, dim, drop_path=0., layer_scale_init_value=1e-6):
+        super().__init__()
+
+        self.mlp = nn.Sequential(
+            nn.GELU(),
+            nn.Linear(64, dim)
+        )
+        self.dwconv = nn.Conv2d(dim*2, dim, kernel_size=7, padding=3, groups=dim) # depthwise conv
+        self.norm = LayerNorm(dim, eps=1e-6)
+        self.pwconv1 = nn.Linear(dim, 4 * dim) # pointwise/1x1 convs, implemented with linear layers
+        self.act = nn.GELU()
+        self.pwconv2 = nn.Linear(4 * dim, dim)
+        self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim)),
+                                    requires_grad=True) if layer_scale_init_value > 0 else None
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.res_conv = nn.Conv2d(dim*2, dim, 1)
+
+    def forward(self, x, time_emb=None):
+        input = x
+        time_emb = self.mlp(time_emb)
+        x = self.dwconv(x) +  rearrange(time_emb, 'b c -> b c 1 1')
+        x = x.permute(0, 2, 3, 1) # (N, C, H, W) -> (N, H, W, C)
+        x = self.norm(x)
+        x = self.pwconv1(x)
+        x = self.act(x)
+        x = self.pwconv2(x)
+        if self.gamma is not None:
+            x = self.gamma * x
+        x = x.permute(0, 3, 1, 2) # (N, H, W, C) -> (N, C, H, W)
+
+        x = self.res_conv(input) + self.drop_path(x)
+        return x
+
+class LKA(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.conv0 = nn.Conv2d(dim, dim, 5, padding=2, groups=dim)
+        self.conv_spatial = nn.Conv2d(dim, dim, 7, stride=1, padding=9, groups=dim, dilation=3)
+        self.conv1 = nn.Conv2d(dim, dim, 1)
+
+
+    def forward(self, x):
+        u = x.clone()
+        attn = self.conv0(x)
+        attn = self.conv_spatial(attn)
+        attn = self.conv1(attn)
+
+        return u * attn
+
+class Attention(nn.Module):
+    def __init__(self, d_model):
+        super().__init__()
+
+        self.proj_1 = nn.Conv2d(d_model, d_model, 1)
+        self.activation = nn.GELU()
+        self.spatial_gating_unit = LKA(d_model)
+        self.proj_2 = nn.Conv2d(d_model, d_model, 1)
+
+    def forward(self, x):
+        shorcut = x.clone()
+        x = self.proj_1(x)
+        x = self.activation(x)
+        x = self.spatial_gating_unit(x)
+        x = self.proj_2(x)
+        x = x + shorcut
+        return x
+
+class Bottleneck(nn.Module):
+    """Bottleneck module
+    Args:
+        inplanes (int): no. input channels
+        planes (int): no. output channels
+        stride (int): stride
+        downsample (nn.Module): downsample module
+    """
+
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = LayerNorm(planes, eps=1e-6, data_format="channels_first")
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = LayerNorm(planes, eps=1e-6, data_format="channels_first") # planes * self.expansion
+        self.conv3 = nn.Conv2d(planes, planes * self.expansion, kernel_size=1, bias=False)
+        self.bn3 = LayerNorm( planes * self.expansion, eps=1e-6, data_format="channels_first")
+        self.relu = nn.SiLU(True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+class Learnable_Filter(nn.Module):
+    """Refinement module of MagNet
+    Args:
+        n_classes (int): no. classes
+        use_bn (bool): use batch normalization on the input
+    """
+
+    def __init__(self, n_classes=1):
+        super().__init__()
+        self.conv1 = nn.Conv2d(640, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 =  LayerNorm(64, eps=1e-6, data_format="channels_first")
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = LayerNorm(64, eps=1e-6, data_format="channels_first")
+        self.relu = nn.SiLU(True)
+
+        # 2 residual blocks
+        self.residual = self._make_layer(Bottleneck, 64, 32, 2)
+        #self.weight_mask_conv = self._make_layer(BasicBlock, 1, 64, 1)
+
+        # Prediction head
+        self.seg_conv = nn.Conv2d(128, 1, kernel_size=1, stride=1, padding=0, bias=False)
+
+    def _make_layer(self, block, inplanes, planes, blocks, stride=1):
+        """Make residual block"""
+        downsample = None
+        if stride != 1 or inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(inplanes, planes * block.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.GroupNorm(2, planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(inplanes, planes, stride, downsample))
+        inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)# + self.weight_mask_conv(weight_mask)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.residual(x)
+
+        return self.seg_conv(x)
